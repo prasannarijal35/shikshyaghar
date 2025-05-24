9652f52ce2b21f85a81d088563a1ba08b57db532
Prasanna Rijal <prasannarijal35@gmail.com>
2025-05-24 11:13:31 +0545

    initial mainpage

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,8 +1,9 @@
+import { MainPage } from "@/components/home";
+
 export default function Home() {
   return (
-    <div className="flex  justify-between items-center container h-[400px]">
-      <div>prasanna le</div>
-      <div>abhinav lai pelyo</div>
-    </div>
+    <>
+      <MainPage />
+    </>
   );
 }
