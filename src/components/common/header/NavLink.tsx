410a1171f67125f130c8ce31cb03b61b549e27ba
Abhinav Gurung <abhinaverog100@gmail.com>
2025-05-24 14:43:07 +0545

    About us

--- src/components/common/header/NavLink.tsx
+++ src/components/common/header/NavLink.tsx
@@ -36,7 +36,7 @@ export default function NavLink({ title, link, dropdowns }: Props) {
           id="dropdownNavbar"
           className="z-10 md:absolute top-[100%] font-normal bg-white divide-y divide-gray-100 shadow-md rounded-lg w-full md:w-[130px] "
         >
-          <ul className="text-[16px] font-medium text-gray-800 w-[130px]  ">
+          <ul className="text-[16px] font-medium text-gray-800 w-full">
             {dropdowns.map((dropdown, index) => (
               <li key={index} className="text-center md:text-left">
                 <Link
