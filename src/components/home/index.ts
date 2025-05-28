e2505a350448d7a706f49e13c26be3366733b827
Prasanna Rijal <prasannarijal35@gmail.com>
2025-05-28 14:22:38 +0545

    hero section to slider

--- src/components/home/index.ts
+++ src/components/home/index.ts
@@ -1,4 +1,4 @@
 export { default as Home } from "./Home";
-export { default as HeroSection } from "./HeroSection";
+export { default as HeroSlider } from "./HeroSlider";
 export { default as OverlapMainpage } from "./OverlapMainpage";
 export { default as Review } from "./Review";
