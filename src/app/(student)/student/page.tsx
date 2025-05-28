c76ca31a06126e53156900d106b2fea259401c0b
Sanjeev Paneru <panerusanjeev6@gmail.com>
2025-05-28 13:40:37 +0545

    managing folders

---
+++ src/app/(student)/student/page.tsx
@@ -0,0 +1,9 @@
+import { StudentDashBoard } from "@/components/studentPannel";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-gray-100">
+      <StudentDashBoard />{" "}
+    </main>
+  );
+}
