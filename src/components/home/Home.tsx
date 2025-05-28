ee65b6e83431581ce3e8dd5ac763d4b991c36117
Prasanna Rijal <prasannarijal35@gmail.com>
2025-05-28 14:22:38 +0545

    hero section to slider

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,18 +1,10 @@
-import { HeroSection } from "@/components/home";
-import banner3 from "@/assets/bannerimages/banner1.jpg";
+import { HeroSlider } from "@/components/home";
 import MainPage from "./MainPage";
 
 export default function Home() {
   return (
     <>
-      <section
-        className="h-screen w-full bg-cover bg-center relative"
-        style={{ backgroundImage: `url(${banner3.src})` }}
-      >
-        <div className="pt-16">
-          <HeroSection />
-        </div>
-      </section>
+      <HeroSlider />
       <MainPage />
     </>
   );
