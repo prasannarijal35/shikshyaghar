a1f5883ef20c194251144964fd9d4d162e330505
Prasanna Rijal <prasannarijal35@gmail.com>
2025-09-08 00:18:15 +0545

    admin pannel student and teacher

--- src/components/adminPanel/students/index.ts
+++ src/components/adminPanel/students/index.ts
@@ -1,2 +1,3 @@
 export { default as StudentTable } from "./StudentTable";
-export { default as DeleteModal } from "./DeleteModal";
+export { default as ViewStudentModal } from "./ViewStudentModal";
+export { default as DeleteConfirmModal } from "./DeleteConfirmModal";
